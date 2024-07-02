dcdad8c9b7f0fba9417d2edce99edb55816be53c
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-07-02 17:27:51 +0200

    many files added

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import Home from '../pages/Home'
+import Services from '../pages/Services'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Contact from '../pages/Contact'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorDetails from '../pages/Doctors/DoctorDetails'
 
 const Routers = () => {
   return (
