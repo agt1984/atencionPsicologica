6718b0ab648b13e62308cec710b99b3c6d754235
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-07-13 12:15:09 +0200

    Home modified with new slogans added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,13 +15,43 @@ const Home = () => {
                     Tu salud, tu bienestar, siempre en línea.
                   </h1>
                   <p className="text__para">
-                    Somos un refugio digital para la salud integral, proporcionando un
-                    puente confiable entre usuarios y profesionales de la salud.
-                    Con un enfoque en la accesibilidad, la calidad y la empatía,
-                    queremos mejorar la vida de nuestros usuarios al
-                    conectarles con los recursos y el apoyo que necesitan para
-                    un bienestar completo, todo desde la comodidad de su hogar.
+                    Somos un refugio digital para la salud integral,
+                    proporcionando un puente confiable entre usuarios y
+                    profesionales de la salud. Con un enfoque en la
+                    accesibilidad, la calidad y la empatía, queremos mejorar la
+                    vida de nuestros usuarios al conectarles con los recursos y
+                    el apoyo que necesitan para un bienestar completo, todo
+                    desde la comodidad de su hogar.
                   </p>
+
+                  <button className="btn">Reserva tu consulta</button>
+                </div>
+
+                {/* === hero counter === */}
+                <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                      +15
+                    </h2>
+                    <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                    <p className="text__para">Años ayudando a personas</p>
+                  </div>
+
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                      +15
+                    </h2>
+                    <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                    <p className="text__para">Años ayudando a personas</p>
+                  </div>
+
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                      +15
+                    </h2>
+                    <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                    <p className="text__para">Años ayudando a personas</p>
+                  </div>
                 </div>
               </div>
             </div>
