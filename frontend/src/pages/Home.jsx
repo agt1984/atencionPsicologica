149a4a9e235b7d23d188e0a260b75b1b622e7925
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-07-17 16:14:48 +0200

    Home modified, new icon added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -115,7 +115,7 @@ const Home = () => {
                   to="/doctors"
                   className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
-                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                  <BsArrowRight className="group-hover:text-white w-6 h-5"  />
                 </Link>
               </div>
             </div>
