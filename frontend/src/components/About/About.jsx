181a7949e20c3f161e21b241535d07151c5bd23b
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-07-21 14:09:37 +0200

    About page added

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import aboutImg from '../../assets/images/about.png' //esta es otra imagen de otro feo, esta tal vez hay que cambiarla
+import React from 'react';
+import aboutImg from '../../assets/images/about.png'; //esta es otra imagen de otro feo, esta tal vez hay que cambiarla
+import aboutCardImg from '../../assets/images/about-card.png'; //aqui ocurre lo mismo, debo crearme una card propia
 
 const About = () => {
   return (
     <section>
         <div className='container'>
             <div className='flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row'>
-
                 {/* ====== about img ====== */}
                 <div className='relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1'>
                     <img src={aboutImg} alt="" />
+                    <div className='absolute z-20 bottom-4 w-[200px] md:w-[300px]'>
+                        <img src={aboutCardImg} alt="" />
+                    </div>
                 </div>
-
             </div>
         </div>
     </section>
-  )
-}
+  );
+};
 
 export default About
