8b6afb007038a6e26a33a04c98ca022aaecd7587
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-07-24 23:26:19 +0200

    Service Card actualiced

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BsArrowRight } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
@@ -18,11 +17,11 @@ const ServiceCard = ({item, index}) => {
       <div className="flex items-center justify-between mt-[30px]">
         <Link
           to="/doctors"
-          className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+          className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] flex items-center justify-center group hover:bg-primaryColor hover:border-none"
         >
           <BsArrowRight className="group-hover:text-white w-6 h-5" />
         </Link>
-        <span className='w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]' style={{background: `${bgColor}`}}>
+        <span className='w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]' style={{background: `${bgColor}`, color:`${textColor}`, borderRadius:'6px 0 0 6px', }}>
           {index + 1}
         </span>
       </div>
