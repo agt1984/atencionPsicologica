e24b0ec6b86f89fb8ff6e8a679a3117821c5dfcc
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-07-28 14:52:13 +0200

    Home actualiced

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -204,7 +204,6 @@ const Home = () => {
               <img src={featureImg} className="w-3/4" alt="" />
 
               <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:pb-[26px] rounded-[10px]">
-
                 <div className="flex items-center justify-between">
                   <div className="flex items-center gap-[6px] lg:gap-3">
                     <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
@@ -214,27 +213,29 @@ const Home = () => {
                       10:00AM
                     </p>
                   </div>
-                  <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]'>
+                  <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
                     <img src={videoIcon} alt="" />
                   </span>
-                
                 </div>
-              
-              <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full'>
-                Consultation
-              </div>
 
-              <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]'>
-                <img src={avatarIcon} alt="" />
-                <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading'></h4>
-              </div>
-              
+                <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
+                  Consultation
+                </div>
+
+                <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
+                  <img src={avatarIcon} alt="" />
+                  <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
+                    Fulano de Tal
+                  </h4>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </section>
       {/* ========= feature section end ========= */}
+      {/* nuestros grandes profesionales */}
+      {/* nuestros grandes profesionales */}
     </>
   );
 };
