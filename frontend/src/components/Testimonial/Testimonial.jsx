41158fcd4074e2498e2f3288aa17ddc984916e83
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-08-17 11:19:02 +0200

    Testimonial component added

---
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Pagination} from "swiper"
+import {Swiper, SwiperSlide} from "swiper/react"
+import "swiper/css"
+import "swiper/css/pagination"
+import patientAvatar from "../../assets/images/patient-avatar.png"
+import {HiStar} from "react-icons/hi"
+
+function Testimonial() {
+  return (
+    <div>Testimonial</div>
+  )
+}
+
+export default Testimonial
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -280,9 +280,9 @@ const Home = () => {
       <section>
         <div className="container">
         <div className="xl:w-[470] mx-auto">
-          <h2 className='heading text-center'>Nuestros grandes doctores</h2>
+          <h2 className='heading text-center'>Que dicen nuestros pacientes</h2>
           <p className='text__para text-center'>
-            Atencion de cuidado psicologco para todos
+            Atencion de cuidado psicologico para todos
           </p>
         </div>
       </div>
