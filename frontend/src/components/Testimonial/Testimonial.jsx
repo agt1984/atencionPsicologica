c1e467d2d2080e51d8ac62de4c141f9f29e35197
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-08-21 08:04:31 +0200

    Testimonial added

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -8,8 +8,42 @@ import {HiStar} from "react-icons/hi"
 
 function Testimonial() {
   return (
-    <div>Testimonial</div>
-  )
+    <div className="mt-[30px] lg:mt-[55px]">
+      <Swiper
+        modules={{ Pagination }}
+        spaceBetween={30}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <div className="py-[30px] px-5 rounded-3">
+            <div className="flex items-center gap-[13px]">
+              <img src={patientAvatar} alt="" />
+              <div>
+                <h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">
+                  Broder con nombre arabe
+                </h4>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default Testimonial
