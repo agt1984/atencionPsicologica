733e446e12366fc894e110d7fa27eb49634ae5e5
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-08-28 06:01:33 +0200

    footer modified

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,32 @@
-import React from 'react'
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import { RiLinkedinFill } from 'react-icons/ri'
+import {
+  AiFillYoutube,
+  AiFillGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+
+const socialLinks = [
+  {
+    path: "https://www.youtube.com/results?search_query=psic%C3%B3loga",
+    icon: <AiFillYoutube className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://www.youtube.com/results?search_query=psic%C3%B3loga",
+    icon: <AiFillGithub className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://www.youtube.com/results?search_query=psic%C3%B3loga",
+    icon: <AiOutlineInstagram className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://www.youtube.com/results?search_query=psic%C3%B3loga",
+    icon: <RiLinkedinFill className="group-hover:text-white w-4 h-5" />,
+  },
+];
 
 const Footer = () => {
   return (
