e310a8bebe0fe18a5aa410eb5d815755533ebe30
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-09-01 19:20:34 +0200

    footer finally ended

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -69,11 +69,11 @@ const quickLinks02 = [
 const quickLinks03 = [
   {
     path: "/",
-    display: "Donate",
+    display: "Dona",
   },
   {
     path: "/contact",
-    display: "Contact Us",
+    display: "Contactanos",
   },
 ];
 
@@ -81,36 +81,86 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className='pb-16 pt-10'>
-      <div className='container'>
-        <div className='flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
+    <footer className="pb-16 pt-10">
+      <div className="container">
+        <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
           <div>
             <img src={logo} alt="" />
-            <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>
-              Copyrigth {year} desarrollado por Alvaro Gonzalez, todos los derechos reservados.
+            <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+              Copyrigth {year} desarrollado por Alvaro Gonzalez, todos los
+              derechos reservados.
             </p>
 
-            <div className='flex items-center gap-3 mt-4'>
-              {socialLinks.map((link, index)=>
-              <Link to={link.path} key={index} className='w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
-                {link.icon}
-              </Link>)}
+            <div className="flex items-center gap-3 mt-4">
+              {socialLinks.map((link, index) => (
+                <Link
+                  to={link.path}
+                  key={index}
+                  className="w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  {link.icon}
+                </Link>
+              ))}
             </div>
           </div>
 
           <div>
-            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
               Quick Links
             </h2>
 
             <ul>
               {quickLinks01.map((item, index) => (
-              <li key={index} className='mb-4'>
-                <Link to={item.path}>{item.display}</Link>
-              </li>
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
               ))}
             </ul>
           </div>
+
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              I want to:
+            </h2>
+
+            <ul>
+              {quickLinks02.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Support
+            </h2>
+
+            <ul>
+              {quickLinks03.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
         </div>
       </div>
     </footer>
