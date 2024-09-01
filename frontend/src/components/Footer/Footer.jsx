a0fef6d8cffff4ab881a20156903f489859d2d87
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-09-01 19:06:43 +0200

    footer modified

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -97,6 +97,16 @@ const Footer = () => {
               </Link>)}
             </div>
           </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+              Quick Links
+            </h2>
+
+            <ul>
+              {quickLinks01.map((item, index)=> <li key={index} className='mb-4'><Link to={item.path}>{item.display}</Link></li>)}
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
