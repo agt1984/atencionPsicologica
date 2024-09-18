91d8d9b690c81f3002c2da475f198e912747100b
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-09-18 11:17:56 +0200

    Doctor Details Modified

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import doctorImg from "../../assets/images/doctor-img02.png"; //atento con esto, mas tarde usaras la direccion para guardar las imagenes personalizadas
 
 const DoctorDetails = () => {
   return (
-    <div>DoctorDetails</div>
-  )
+    <section>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        <div className="grid md:grid-cols-3 gap-[50px]">
+          <div className="md:col-span-2">
+            <div className="flex items-center gap-5">
+              <figure className='m-w-[200px] max-h-[200px]'>
+                <img src={doctorImg} alt="" className='w-full'/>
+              </figure>
+
+              <div>
+                <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px]'>Surgeon</span>
+              </div>
+            </div>
+          </div>
+
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default DoctorDetails;
